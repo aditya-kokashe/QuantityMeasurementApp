4eabf866d99ae7f7381db0e67b6d93142e09062e
aditya-kokashe <adi310kokashe@gmail.com>
2026-03-17 00:36:57 +0530

    [Aditya Kokashe] : added QuantityMeasurementApp.java with updated application layer workflow

--- src/main/java/com/apps/quantitymeasurement/application/QuantityMeasurementApp.java
+++ src/main/java/com/apps/quantitymeasurement/application/QuantityMeasurementApp.java
@@ -1,63 +1,69 @@
+package com.apps.quantitymeasurement.application;
 
-	package com.apps.quantitymeasurement.application;
+import java.sql.Connection;
+import java.sql.ResultSet;
+import java.sql.SQLException;
+import java.sql.Statement;
 
-	import com.apps.quantitymeasurement.controller.QuantityMeasurementController;
-	import com.apps.quantitymeasurement.model.QuantityDTO;
-	import com.apps.quantitymeasurement.repository.QuantityMeasurementCacheRepository;
-	import com.apps.quantitymeasurement.service.QuantityMeasurementServiceImpl;
+import com.apps.quantitymeasurement.model.QuantityDTO;
+import com.apps.quantitymeasurement.repository.IQuantityMeasurementRepository;
+import com.apps.quantitymeasurement.repository.QuantityMeasurementCacheRepository;
+import com.apps.quantitymeasurement.repository.QuantityMeasurementDatabaseRepository;
+import com.apps.quantitymeasurement.service.QuantityMeasurementServiceImpl;
+import com.apps.quantitymeasurement.util.ApplicationConfig;
+import com.apps.quantitymeasurement.util.ConnectionPool;
 
-	public class QuantityMeasurementApp {
 
-	    public static void main(String[] args) {
+public class QuantityMeasurementApp {
 
-	        // Repository
-	        QuantityMeasurementCacheRepository repository =
-	                new QuantityMeasurementCacheRepository();
+    public static void main(String[] args) throws SQLException {
 
-	        // Service
-	        QuantityMeasurementServiceImpl service =
-	                new QuantityMeasurementServiceImpl(repository);
+        IQuantityMeasurementRepository repository;
 
-	        // Controller
-	        QuantityMeasurementController controller =
-	                new QuantityMeasurementController(service);
+        String repositoryType = ApplicationConfig.getProperty("repository.type");
+        System.out.println("Repository Type: " + repositoryType);
 
-	        // Example 1: Comparison
-	        QuantityDTO q1 =
-	                new QuantityDTO(1, QuantityDTO.LengthUnit.FEET);
+        if(repositoryType != null && repositoryType.trim().equalsIgnoreCase("database")) {
+            repository = new QuantityMeasurementDatabaseRepository();
+        }else {
+            repository = QuantityMeasurementCacheRepository.getInstance();
+        }
 
-	        QuantityDTO q2 =
-	                new QuantityDTO(12, QuantityDTO.LengthUnit.INCHES);
+        // Use dependency injection properly
+        QuantityMeasurementServiceImpl service =
+                new QuantityMeasurementServiceImpl(repository);
 
-	        System.out.println("1 FEET equals 12 INCHES: "
-	                + controller.compare(q1, q2));
+        // Create quantities
+        QuantityDTO q1 = new QuantityDTO(1, QuantityDTO.LengthUnit.FEET);
+        QuantityDTO q2 = new QuantityDTO(12, QuantityDTO.LengthUnit.INCHES);
 
-	        // Example 2: Conversion
-	        QuantityDTO converted =
-	                controller.convert(q1, QuantityDTO.LengthUnit.INCHES);
+        // Perform operations
+        service.add(q1, q2);
+        service.subtract(q1, q2);
+        service.divide(q1, q2);
 
-	        System.out.println("1 FEET in INCHES: "
-	                + converted.value);
+        Connection conn = ConnectionPool.getConnection();
+        Statement stmt = conn.createStatement();
 
-	        // Example 3: Addition
-	        QuantityDTO added =
-	                controller.add(q1, q2);
+        System.out.println("Tables in Database:");
 
-	        System.out.println("Addition result: "
-	                + added.value + " " + added.unit.getUnitName());
+        ResultSet tables = stmt.executeQuery("SHOW TABLES");
+        while (tables.next()) {
+            System.out.println(tables.getString(1));
+        }
 
-	        // Example 4: Subtraction
-	        QuantityDTO subtracted =
-	                controller.subtract(q1, q2);
+        System.out.println("\nStored Measurements:");
 
-	        System.out.println("Subtraction result: "
-	                + subtracted.value + " " + subtracted.unit.getUnitName());
+        ResultSet rs = stmt.executeQuery("SELECT * FROM QUANTITY_MEASUREMENT_ENTITY");
 
-	        // Example 5: Division
-	        double result =
-	                controller.divide(q1, new QuantityDTO(2,
-	                        QuantityDTO.LengthUnit.FEET));
-
-	        System.out.println("Division result: " + result);
-	    }
-	}
+        while (rs.next()) {
+            System.out.println(
+                    rs.getLong("id") + " | " +
+                    rs.getString("operation") + " | " +
+                    rs.getString("operand1") + " | " +
+                    rs.getString("operand2") + " | " +
+                    rs.getString("result")
+            );
+        }
+    }
+}
