b3284db105d922d681ffde9757b130f51eb32f10
aditya-kokashe <adi310kokashe@gmail.com>
2026-03-24 11:58:20 +0530

    [Aditya Kokashe] : added UC17 QuantityMeasurementRepository.java program handling data access operations

---
+++ src/main/java/com/apps/quantitymeasurement/repository/QuantityMeasurementRepository.java
@@ -0,0 +1,15 @@
+package com.apps.quantitymeasurement.repository;
+
+import java.util.List;
+
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
+
+import com.apps.quantitymeasurement.model.QuantityMeasurementEntity;
+
+@Repository
+public interface QuantityMeasurementRepository extends JpaRepository<QuantityMeasurementEntity, Long> {
+
+	List<QuantityMeasurementEntity> findByOperation(String operation);
+
+}
