e08e6b6e75efc851802fa1fbb17b13131b7ac777
aditya-kokashe <adi310kokashe@gmail.com>
2026-03-24 11:58:23 +0530

    [Aditya Kokashe] : added UC17 QuantityMeasurementAppTest.java program testing application functionality

---
+++ src/test/java/com/apps/quantitymeasurement/QuantityMeasurementAppTest.java
@@ -0,0 +1,13 @@
+package com.apps.quantitymeasurement;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class QuantityMeasurementAppTest {
+
+    @Test
+    void contextLoads() {
+        System.out.println("Spring Boot application started successfully");
+    }
+}
