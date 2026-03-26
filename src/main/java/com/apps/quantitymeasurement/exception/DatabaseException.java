98924f7cc390e98b52ec73ff963869b8396d05c1
aditya-kokashe <adi310kokashe@gmail.com>
2026-03-26 15:06:53 +0530

    [Aditya Kokashe] : added UC18 DatabaseException.java with updated exception handling

--- src/main/java/com/apps/quantitymeasurement/exception/DatabaseException.java
+++ src/main/java/com/apps/quantitymeasurement/exception/DatabaseException.java
@@ -1,11 +1,28 @@
 package com.apps.quantitymeasurement.exception;
 
-public class DatabaseException extends RuntimeException {
+public class DatabaseException extends QuantityMeasurementException {
 
-    public DatabaseException(String message, Throwable cause) {
+	private static final long serialVersionUID = 1L;
 
-        super(message, cause);
+	public DatabaseException(String msg) {
+		super(msg);
+	}
+	
+	public DatabaseException(String message, Throwable cause) {
+		super(message, cause);
+	}
 
-    }
+	public static DatabaseException connectionFailed(String details, Throwable cause) {
+		return new DatabaseException("Database connection failed: " + details, cause);
 
+	}
+
+	public static DatabaseException queryFailed(String query, Throwable cause) {
+		return new DatabaseException("Query execution failed: " + query, cause);
+
+	}
+
+	public static DatabaseException transactionFailed(String operation, Throwable cause) {
+		return new DatabaseException("Transaction failed during " + operation, cause);
+	}
 }
