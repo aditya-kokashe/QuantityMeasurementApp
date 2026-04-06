0b091780cde53acecc1e15171d619e22c2a63675
aditya-kokashe <adi310kokashe@gmail.com>
2026-04-06 14:40:58 +0530

    [Aditya Kokashe] : added QuantityMeasurementServiceImpl.java with updated business logic implementation

--- src/main/java/com/apps/quantitymeasurement/service/QuantityMeasurementServiceImpl.java
+++ src/main/java/com/apps/quantitymeasurement/service/QuantityMeasurementServiceImpl.java
@@ -20,6 +20,7 @@ import com.apps.quantitymeasurement.unit.WeightUnit;
 
 import java.util.List;
 import java.util.logging.*;
+
 @Service
 public class QuantityMeasurementServiceImpl implements IQuantityMeasurementService{
 	// Logger for logging information and errors
