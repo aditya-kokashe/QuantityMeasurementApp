af97373b138fb56a42c3b3d41dbbff1eb40e166a
aditya-kokashe <adi310kokashe@gmail.com>
2026-04-06 14:40:55 +0530

    [Aditya Kokashe] : added QuantityMeasurementController.java with updated API handling logic

--- src/main/java/com/apps/quantitymeasurement/controller/QuantityMeasurementController.java
+++ src/main/java/com/apps/quantitymeasurement/controller/QuantityMeasurementController.java
@@ -1,25 +1,25 @@
 package com.apps.quantitymeasurement.controller;
 
+import java.util.List;
+import java.util.logging.Logger;
+
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 import com.apps.quantitymeasurement.dto.QuantityInputDTO;
 import com.apps.quantitymeasurement.dto.QuantityMeasurementDTO;
-import com.apps.quantitymeasurement.service.IQuantityMeasurementService;
 import com.apps.quantitymeasurement.service.QuantityMeasurementServiceImpl;
+
 import io.swagger.v3.oas.annotations.Operation;
 import io.swagger.v3.oas.annotations.media.Content;
 import io.swagger.v3.oas.annotations.media.ExampleObject;
-import org.springframework.web.bind.annotation.RequestBody;
 import io.swagger.v3.oas.annotations.tags.Tag;
 import jakarta.validation.Valid;
-
-import java.util.List;
-import java.util.logging.*;
 @RestController
 @RequestMapping("api/v1/quantities")
 @Tag(name = "Quantity Measurements", description = "REST API for quantity measurement operations")
@@ -37,7 +37,7 @@ public class QuantityMeasurementController {
 		this.quantityMeasurementService = quantityMeasurementService;
 	}
 	
-	// - Reusable json example snippets
+	// - Reusable JSON example SNIPPETS
 	private static final String EX_FEET_INCH ="""
 		{ 
 			"thisQuantityDTO": {"value":1.0,"unit":"FEET", "measurementType":"LengthUnit"},
@@ -69,13 +69,15 @@ public class QuantityMeasurementController {
 			"targetQuantityDTO": {"value":0.0, "unit":"INCHES", "measurementType":"LengthUnit"} 
 		}""";
 	
-	/*
-	 * Welcome Get Mapping Testing
-	 */
+	
+	
+	//GET MAPPING
 	@GetMapping()
 	public String welcome() {
 		return "Welcome! It is working";
 	}
+	
+	//POST MAPPING 
 	@PostMapping("/compare")
 	@Operation(summary = "Compare two quantities",
 		requestBody = @io.swagger.v3.oas.annotations.parameters.RequestBody(
